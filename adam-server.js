ffdb78177337fda6660935e34c346824ffd7638d
Adam Patterson <awpatterson217@gmail.com>
2017-08-09 20:13:40 -0500

    test file

--- adam-server.js
+++ adam-server.js
@@ -5,26 +5,28 @@ const app        = express();
 
 const port = 3000;
 
-const static   = require('./pub/static');
-const index    = require('./pub/routes/index');
+//const static   = require('./pub/static');
+//const index    = require('./pub/routes/index');
 
 //View Engine
-app.set('views', path.join(__dirname, './pub/static/views'));
-app.set('view engline', 'ejs');
+//app.set('views', path.join(__dirname, './pub/static/views'));
+//app.set('view engline', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
 // Set Static Folder
-app.use(express.static(path.join(__dirname, 'client')));
+//app.use(express.static(path.join(__dirname, 'client')));
 
 // Body Parser MW
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', index);
-app.use('/', express.static(__dirname + static + '/css/'));
-app.use('/', express.static(__dirname + static + '/img/'));
-app.use('/', express.static(__dirname + static + '/js/'));
-app.use('/', express.static(__dirname + static + '/external/'));
+app.use('/test', 'test.js');
+
+//app.use('/', express.static(__dirname + static + '/css/'));
+//app.use('/', express.static(__dirname + static + '/img/'));
+//app.use('/', express.static(__dirname + static + '/js/'));
+//app.use('/', express.static(__dirname + static + '/external/'));
 
 const server = app.listen(port, () => {
 
