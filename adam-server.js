5a1bb05134cacdda783e7962c4ce6053d74c5c8c
Adam Patterson <awpatterson217@gmail.com>
2017-09-03 19:19:16 -0500

    Adding Redis

--- adam-server.js
+++ adam-server.js
@@ -1,49 +1,69 @@
 const express    = require('express');
 const bodyParser = require('body-parser');
 const helmet     = require('helmet');
+const session    = require('express-session');
+const RedisStore = require('connect-redis')(session);
 
+/**
+ * Initializations
+ */
 const port = 3000;
-const app = express();
-app.use(helmet());
+const app  = express();
 
-const options = {
+//        OPTIONS
+
+const defaultGetOptions = {
   root: __dirname + '/public/',
   dotfiles: 'deny',
   headers: {
       'x-timestamp': Date.now(),
       'x-sent': true
   }
-};
+}
 
-app.get('/test', function (req, res, next) {
+const redisOptions = {
+  port: 6379
+}
 
-  //let target = req.params.name;
-  res.sendFile('index.html', options, function (err) {
-    if (err) 
-      next(err);
-    else 
-      console.log('Sent:', target);
-  });
+//        MIDDLEWARE
 
-});
+app.use(helmet());
 
-app.get('/some_data', function (req, res, next) {
-  console.log('GET request to /some_data.')
-  next()
-}, function (req, res) {
-  res.send('GET request to /some_data.')
-})
+app.use(
+  session({
+    store: new RedisStore(redisOptions),
+    secret: 'keyboard cat'
+  })
+);
 
 //app.use('/', express.static(__dirname + 'public/static/css/'));
 //app.use('/', express.static(__dirname + 'public/static/img/'));
 //app.use('/', express.static(__dirname + 'public/static/js/'));
 //app.use('/', express.static(__dirname + 'public/static/external/'));
 
-const server = app.listen(port, () => {
+//        TESTING
 
+app.get('/test', function (req, res, next) {
+  let target = req.params.name;
+  res.sendFile('index.html', defaultGetOptions, function (err) {
+    if (err)
+      next(err);  
+    else
+      console.log('Sent:', target);
+  });
+});
+
+app.get('/some_data', function (req, res, next) {
+  console.log('GET request to /some_data.');
+  next();
+}, function (req, res) {
+  res.send('GET request to /some_data.');
+});
+
+//        SERVER
+
+const server = app.listen(port, () => {
     const host = server.address().address;
     const port = server.address().port;
-
     console.log(`Server running at http://${host}:${port}`);
-
 });
