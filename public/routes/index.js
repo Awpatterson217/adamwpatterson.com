58e69b82c8d4cd369a1517430b654b270f56b0ad
Adam Patterson <awpatterson217@gmail.com>
2017-09-05 15:15:06 -0500

    Adding links

--- public/routes/index.js
+++ public/routes/index.js
@@ -1,16 +1,19 @@
 "use strict";
 
 module.exports = function(app) {
- app.get('/home', function(req, res) {
-   res.render('index');
- });
- app.get('/about', function(req, res) {
-   res.render('about');
- });
- app.get('/blog', function(req, res) {
-    res.render('blog');
+  app.get('/home', function(req, res) {
+    res.render('index');
   });
   app.get('/portfolio', function(req, res) {
     res.render('portfolio');
+  });
+  app.get('/about', function(req, res) {
+    res.render('about');
+  });
+  app.get('/contact', function(req, res) {
+    res.render('contact');
+  });
+  app.get('/blog', function(req, res) {
+    res.render('blog');
   });
 };
