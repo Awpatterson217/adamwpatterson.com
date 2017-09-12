1d2bf5d2c3e68b706190a53cb8ee79b43c2dc66e
Adam Patterson <awpatterson217@gmail.com>
2017-09-12 02:23:19 -0500

    Inverting colors

--- public/resources/js/activeLinks.js
+++ public/resources/js/activeLinks.js
@@ -1,16 +1,17 @@
 const currentPage = window.location.pathname.slice(1);    
 
 let removeAllActive = function(){
-    document.getElementById("portfolio").classList.remove("active");
-    document.getElementById("about").classList.remove("active");
-    document.getElementById("contact").classList.remove("active");
-    document.getElementById("blog").classList.remove("active");
+	document.getElementById("portfolio").classList.remove("active");
+	document.getElementById("about").classList.remove("active");
+	document.getElementById("contact").classList.remove("active");
+	document.getElementById("blog").classList.remove("active");
 }
 
 let addActive = function(){
-    if (typeof(currentPage) !== 'undefined' && currentPage !== null && currentPage !== ''){
-        document.getElementById(`${currentPage}`).classList.add("active");
-    }
+	if (typeof(currentPage) !== 'undefined' && currentPage !== null && currentPage !== ''){
+		if(currentPage !== 'home')            
+			document.getElementById(`${currentPage}`).classList.add("active");
+	}
 }
 
 removeAllActive();
